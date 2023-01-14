2b458b1bda9e707b1c9f58f7588148b73da38ddf
Ashutosh Kumar Patel <ashutosh.kumar.patel.2003@gmail.com>
2023-01-14 12:01:00 +0530

    Landing Page progress

--- Tracker/models.py
+++ Tracker/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# from django.db import models
+from djongo import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    url = models.URLField(null = False)
+    price_history = models.TextField()
+    notify_user = models.BooleanField(default=1)
+    # User = 
+
+class Price_History(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    price = models.FloatField()
---
+++ static/js/landing.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const HiddenElements = document.querySelectorAll('.hidden');
+HiddenElements.forEach((e1) => observer.observe(e1));

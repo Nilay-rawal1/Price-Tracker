341a0ca9d47a8bd460c4e1f89c7ba94a43ff933c
Ashutosh Kumar Patel <Ashutosh.kumar.patel.2003@gmail.com>
2023-01-18 22:46:34 +0530

    Test Deploy to Railway

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guicorn PriceTracker.wsgi --log-file -

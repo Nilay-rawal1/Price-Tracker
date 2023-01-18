363bf6df65e80f96f97e9c900e31f0be550a8f9a
Ashutosh Kumar Patel <Ashutosh.kumar.patel.2003@gmail.com>
2023-01-19 00:49:44 +0530

    Added domain name

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['barcost.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
